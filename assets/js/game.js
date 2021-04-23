1abbae6a8bb37b758fbbd2d94a4e20b8d0a0663b
Gonzalo Martinese <devmartinese@gmail.com>
2021-04-23 02:51:59 -0300

    actualizacion de codigo

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,23 +1,39 @@
+
 let deck = [];
-let types = ['C', 'D', 'H', 'S'];
-let especials = ['A', 'J', 'Q', 'K'];
+let types = ["C", "D", "H", "S"];
+let especials = ["A", "J", "Q", "K"];
 
 const createDeck = () => {
-  for(let i = 2; i <= 10; i++) {
-    for(let type of types) {
-      deck.push(`${i + type}`)
+  for (let i = 2; i <= 10; i++) {
+    for (let type of types) {
+      deck.push(`${i + type}`);
     }
-  };
+  }
 
   for (let type of types) {
     for (let esp of especials) {
-      deck.push(`${esp + type}`)
+      deck.push(`${esp + type}`);
     }
   }
 
   deck = _.shuffle(deck);
-  console.log(deck);
+  // console.log(deck)
   return deck;
 };
 
 createDeck();
+
+const getCard = () => {
+  if (deck.length === 0) throw "No hay cartas";
+
+  const card = deck.pop();
+
+  console.log(deck);
+  console.log(card);
+
+  return card;
+};
+
+for (let i = 0; i <= 100; i++) {
+  getCard();
+}
